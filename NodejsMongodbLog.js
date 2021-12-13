93aed54c76f2d4160db6bf345ae720b89fd31533
shockdee <1062740339@qq.com>
2021-12-12 17:26:38 -0800

    mongodb success form web.

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,11 +1,24 @@
 const express = require("express")
 const app = express()
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://172.21.2.236:27017/190110910823');
+const schema = {
+    name: String,
+    age: Number,
+    health: String,
+    hobby:String,
+}
+const mydata = mongoose.model('cat1s', schema);
+// const kitty = new mydata({name:'test1'});
+// kitty.save()
 
 app.use('/',express.static('public'))
-
 app.get("/input",(req,res)=>{
     res.send(req.query)
     console.log(req.query)
+    const kitty = new mydata({name:req.query.first,health:req.query.second});
+    kitty.save()
 })
 
 app.listen(21921)
